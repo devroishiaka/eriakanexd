d4da13ed14015c72438dd4418e1801b56bda493b
Rohan Jaishwal <108065240+devroishiaka@users.noreply.github.com>
2022-12-02 10:48:14 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+TOKEN = 
+PORT = int(os.environ.get('PORT', 5000))
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Helllloooo!')
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0",
+                            port=int(PORT),
+                            url_path=TOKEN)
+    updater.bot.setWebhook('https://testingerixd.herokuapp.com/' + TOKEN)
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
